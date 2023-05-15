c7d29bd3ac336d780a384cb89c41a0bf75214585
Raahil <raahil2005@hotmail.com>
2023-05-16 04:05:11 +0500

    init first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<div>
+  hello sex3
+</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
